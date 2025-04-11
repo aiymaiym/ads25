d6b17d57eb338f59e689162bb528d77a0bc61ff9
aiymaiym <your_email@example.com>
2025-04-11 20:01:59 +0500

    Implement MyArrayList with all required methods

---
+++ src/MyArrayList.java
@@ -0,0 +1,71 @@
+public class MyArrayList<T> extends MyList<T> {
+    private Object[] data;
+    private int size;
+
+    public MyArrayList() {
+        data = new Object[10];
+        size = 0;
+    }
+
+    @Override
+    public void add(T element) {
+        ensureCapacity();
+        data[size++] = element;
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        return (T) data[index];
+    }
+
+    @Override
+    public T remove(int index) {
+        checkIndex(index);
+        T removed = (T) data[index];
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        data[--size] = null;
+        return removed;
+    }
+
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+
+    private void ensureCapacity() {
+        if (size == data.length) {
+            Object[] newData = new Object[data.length * 2];
+            System.arraycopy(data, 0, newData, 0, data.length);
+            data = newData;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<T>() {
+            private int index = 0;
+            public boolean hasNext() {
+                return index < size;
+            }
+            public T next() {
+                return (T) data[index++];
+            }
+        };
+    }
+}
+

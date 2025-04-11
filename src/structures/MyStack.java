9605a56995e18afd26b86cb8cb2f1244efc486c5
aiymaiym <your_email@example.com>
2025-04-11 20:11:57 +0500

    Implement MyStack using MyArrayList

--- src/structures/MyStack.java
+++ src/structures/MyStack.java
@@ -1,4 +1,32 @@
 package structures;
 
-public class MyStack {
+public class MyStack<T> {
+    private MyArrayList<T> list;
+
+    public MyStack() {
+        list = new MyArrayList<>();
+    }
+    public void push(T element) {
+        list.add(element);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return list.remove(list.size() - 1);
+    }
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return list.get(list.size() - 1);
+    }
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+    public int size() {
+        return list.size();
+    }
 }
+

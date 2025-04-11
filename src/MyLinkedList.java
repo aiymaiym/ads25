23aa440abbb30f2264c70b27c6fdb7a33f1d9851
aiymaiym <your_email@example.com>
2025-04-11 20:05:57 +0500

    Commiting MyLinkedList with all methods

---
+++ src/MyLinkedList.java
@@ -0,0 +1,104 @@
+public class MyLinkedList<T> implements MyList<T> {
+    private class MyNode {
+        T data;
+        MyNode next;
+        MyNode prev;
+
+        MyNode(T data) {
+            this.data = data;
+        }
+    }
+
+    private MyNode head;
+    private MyNode tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public void add(T element) {
+        MyNode node = new MyNode(element);
+        if (head == null) {
+            head = tail = node;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+        size++;
+    }
+
+    @Override
+    public T get(int index) {
+        return getNode(index).data;
+    }
+
+    @Override
+    public T remove(int index) {
+        MyNode toRemove = getNode(index);
+
+        if (toRemove.prev != null) {
+            toRemove.prev.next = toRemove.next;
+        } else {
+            head = toRemove.next;
+        }
+
+        if (toRemove.next != null) {
+            toRemove.next.prev = toRemove.prev;
+        } else {
+            tail = toRemove.prev;
+        }
+
+        size--;
+        return toRemove.data;
+    }
+
+    private MyNode getNode(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+
+        MyNode current;
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+        }
+        return current;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<T>() {
+            private MyNode current = head;
+
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            public T next() {
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
+}

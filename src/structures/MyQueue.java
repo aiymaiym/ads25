db08e010849476c4e22fb9cd7367b21cd162ff5b
aiymaiym <your_email@example.com>
2025-04-11 20:13:23 +0500

    Implement MyQueue using MyLinkedList

--- src/structures/MyQueue.java
+++ src/structures/MyQueue.java
@@ -1,4 +1,32 @@
 package structures;
 
-public class MyQueue {
+public class MyQueue<T> {
+    private MyLinkedList<T> list;
+
+    public MyQueue() {
+        list = new MyLinkedList<>();
+    }
+    public void enqueue(T element) {
+        list.add(element);
+    }
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return list.remove(0);
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return list.get(0);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+    public int size() {
+        return list.size();
+    }
 }

a45de78c781fcce55de00f2f3459853c6c42319e
aiymaiym <your_email@example.com>
2025-04-27 11:33:51 +0500

    Initial Commmit

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main {
         arrayList.add(10);
         arrayList.add(20);
         arrayList.add(30);
-        System.out.println("ArrayList get(1): " + arrayList.get(1)); // 20
+        System.out.println("ArrayList get(1): " + arrayList.get(1));
         arrayList.remove(0);
-        System.out.println("ArrayList after remove: " + arrayList.get(0)); // 20
+        System.out.println("ArrayList after remove: " + arrayList.get(0));
 
         // MyLinkedList
         MyList<String> linkedList = new MyLinkedList<>();
@@ -19,7 +19,7 @@ public class Main {
         linkedList.add("three");
         linkedList.remove(1);
         for (String s : linkedList) {
-            System.out.println("LinkedList element: " + s); // one, three
+            System.out.println("LinkedList element: " + s);
         }
 
         // MyStack
@@ -27,16 +27,16 @@ public class Main {
         stack.push(1);
         stack.push(2);
         stack.push(3);
-        System.out.println("Stack pop: " + stack.pop()); // 3
-        System.out.println("Stack peek: " + stack.peek()); // 2
+        System.out.println("Stack pop: " + stack.pop());
+        System.out.println("Stack peek: " + stack.peek());
 
         // MyQueue
         MyQueue<String> queue = new MyQueue<>();
         queue.enqueue("A");
         queue.enqueue("B");
         queue.enqueue("C");
-        System.out.println("Queue dequeue: " + queue.dequeue()); // A
-        System.out.println("Queue peek: " + queue.peek()); // B
+        System.out.println("Queue dequeue: " + queue.dequeue());
+        System.out.println("Queue peek: " + queue.peek());
 
         // MyMinHeap
         MyMinHeap<Integer> heap = new MyMinHeap<>();
@@ -44,7 +44,28 @@ public class Main {
         heap.add(2);
         heap.add(8);
         heap.add(1);
-        System.out.println("Heap poll: " + heap.poll()); // 1
-        System.out.println("Heap peek: " + heap.peek()); // 2
+        System.out.println("Heap poll: " + heap.poll());
+        System.out.println("Heap peek: " + heap.peek());
+        TestHash();
+    }
+    // TestHashTable - testing put, get, remove, contains, getK
+    public static void TestHash(){
+        MyHashTable<String, Integer> table = new MyHashTable<>();
+        //put
+        table.put("Arman", 18);
+        table.put("Alina", 21);
+        //get
+        System.out.println("Arman's age: " + table.get("Arman" ));
+        System.out.println("Alina's age: " + table.get("Alina" ));
+        //contains
+        System.out.println("Contains 18: " + table.contains(18));
+        System.out.println("Contains 30: " + table.contains(30));
+        //getKey
+        System.out.println("Key for value 21: " + table.getKey(21));
+        System.out.println("Key for value 30: " + table.getKey(30));
+        //remove
+        System.out.println("Remove Arman: " + table.remove("Arman"));
+        System.out.println("After remove Arman: " + table.get("Arman"));
+        System.out.println("Contains 18 after remove: " + table.contains(18));
     }
 }
